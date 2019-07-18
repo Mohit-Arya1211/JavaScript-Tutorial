e465702e1af13886f3c1b6fd6b2daccd1ea21b82
Mohit-Arya1211 <mohit.arya1211@gmail.com>
2019-07-19 00:13:29 +0530

    Added Code

--- Product Available/script.js
+++ Product Available/script.js
@@ -1,7 +1,18 @@
+//0 Product List :
+var products = "apple  blackberry pumpkin broccoli banana tomato";
 // 1- Show a Pop Up And  Get the input for what user want:
+var whatToDo = prompt("Type '1' to check is the product is available \n, Type '2' to get the list of all the products \n , type anything else to quit .");
+console.log(whatToDo);
   /* 1.2 - If User Do not want to Search :
      If the User type "2" , We will display all the product :
 */ 
+if(whatToDo==='2'){
+    console.log();
+    document.write("<p class=Info> The Products available in store are : "  + products "</p>" );
+}
+else if (whatToDo==='1'){
+    
+}
 
 /* 1.3- If the user types "1", the page will display a pop up to get the name of the product,
 -if the product ,is not available 
