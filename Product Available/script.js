1888b139d8e458b44410e86164fa5ddda2a0d410
Mohit-Arya1211 <mohit.arya1211@gmail.com>
2019-07-19 00:04:00 +0530

    Added Comments

--- Product Available/script.js
+++ Product Available/script.js
@@ -1,3 +1,13 @@
 // 1- Show a Pop Up And  Get the input for what user want:
-  // 1.1 - If User want to Search :
-  // 1.2 - If User Do not want to Search :
+  /* 1.2 - If User Do not want to Search :
+     If the User type "2" , We will display all the product :
+*/ 
+
+/* 1.3- If the user types "1", the page will display a pop up to get the name of the product,
+-if the product ,is not available 
+display the Error Message:
+-else display the Success message for the vailable product:
+*/
+
+/*1.4- if the user types in anything else , we will display "Refresh the page to reload the list of all the products available.
+*/
