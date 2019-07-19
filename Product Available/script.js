51396bcfd3cee74c11ba8ef5c777b187a04a9ce5
Mohit-Arya1211 <mohit.arya1211@gmail.com>
2019-07-19 15:31:02 +0530

    Working

--- Product Available/script.js
+++ Product Available/script.js
@@ -1,24 +1,30 @@
-//0 Product List :
-var products = "apple  blackberry pumpkin broccoli banana tomato";
-// 1- Show a Pop Up And  Get the input for what user want:
-var whatToDo = prompt("Type '1' to check is the product is available \n, Type '2' to get the list of all the products \n , type anything else to quit .");
-console.log(whatToDo);
-  /* 1.2 - If User Do not want to Search :
-     If the User type "2" , We will display all the product :
-*/ 
-if(whatToDo==='2'){
-    console.log();
-    document.write("<p class=Info> The Products available in store are : "  + products "</p>" );
-}
-else if (whatToDo==='1'){
+var products = "apple blackberry banana tomato broccoli pumpkin";
+//1- Show a popup and get what the user wants
+var whatToDo = prompt("Type '1' to check if a product is available\nType '2' to list all products\nType anything to quit");
+/*1-1 if the user types in "2"
+we will list all the products
+*/
+if (whatToDo === '2') {
+    document.write("<p class=info>The products available: " + products + "</p>");
+} else if (whatToDo === "1") {
+    /*1-2 if the user types in "1"
+        1-2-1 display another popup to get the name of the product.
+     */  
+    var product = prompt("Enter the name of the product to see if it's available");
+    /*
+        -if he looks for a product that is not available, display sorry the product is not available
+        -else(the product is available), we display the product is available
+    */
+    if(products.indexOf(product) === -1){
+        document.write("<p class=error>Sorry, the product is not available</p>");
+    }else{
+        document.write("<p class=success>The product is available</p>");
+    }
     
+}else{
+    document.write("<p class=info>Refresh the page if you want to look for a product or list all the products</p>");
 }
 
-/* 1.3- If the user types "1", the page will display a pop up to get the name of the product,
--if the product ,is not available 
-display the Error Message:
--else display the Success message for the vailable product:
-*/
 
-/*1.4- if the user types in anything else , we will display "Refresh the page to reload the list of all the products available.
-*/
+/*1-3 if he types in anything, we will display "Refresh the page if you want to look for a product or list all the products"
+ */
